5d2570af255d45dc95aff77645b6a94839f3aae7
Sarkor2007 <abilkosimovsarkor@gmail.com>
2023-03-15 18:56:10 +0500

    add detail page

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 import * as functions from "./modules/functions.js";
+import * as header from "./modules/header.js";
+import * as detail from "./modules/detail.js";
 
 functions.isWebp();
+header.headerFn();
+detail.detailFn();
 
 
